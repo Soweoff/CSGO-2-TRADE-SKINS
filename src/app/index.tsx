2a30e4c33f6adbba6067b2d871728d188029cf3a
Soweoff <evertonsouzawience@gmail.com>
2025-05-14 21:21:24 -0300

    Atualização do projeto

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,13 +5,6 @@ export default function Home() {
   return (
     <View style={{ padding: 20 }}>
       <Text>Página Inicial</Text>
-      <Link href="/tela1">Ir para Tela 1</Link>
-      <Link href="/tela2">Ir para Tela 2</Link>
-      <Link href={`/param/${Math.floor(Math.random() * 100)}`}>
-        Ir para tela com parâmetro
-      </Link>
-      <Link href="/com-botao">Ir para tela com botão</Link>
-      <Link href="/tabs">Ir para Tabs</Link>
     </View>
   );
 }
